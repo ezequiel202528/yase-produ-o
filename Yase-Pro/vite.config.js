166a080f46d956dcbb728b1cb6263cf5d986ecb7
ezequiel <ezequiel2023es@gmail.com>
2026-04-11 13:33:28 -0300

    ajsute do vite para implatação do vercel

--- Yase-Pro/vite.config.js
+++ Yase-Pro/vite.config.js
@@ -1,11 +1,25 @@
+// import { defineConfig } from "vite";
+// import { resolve } from "path";
+
+// export default defineConfig({
+//   root: "minhaApp-win32-x64/resources/app",
+//   base: "./",
+//   build: {
+//     outDir: "../../../dist/app",
+//     emptyOutDir: true,
+//   },
+//   server: {
+//     port: 5173,
+//   },
+// });
+
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
-  root: "minhaApp-win32-x64/resources/app",
+  root: "./",
   base: "./",
   build: {
-    outDir: "../../../dist/app",
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
